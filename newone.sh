e7d04762ac4c8922a63197584c3ab1586f78cdb8
nithin1217 <108689346+nithin1217@users.noreply.github.com>
2024-03-08 01:33:52 +0530

    Update and rename newone to newone.sh

---
+++ newone.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#'continue' statement in a loop
+for n in {1..4}; do
+if [ $n -eq 2 ]; then
+echo "Iteration skipped for n = $n"
+continue #Skipping rest of the code in this iteration
+fi
+echo "Iteration $n"
+done
