8660e66dce49325966c6b69ec1e92bb53a346c31
nithin1217 <108689346+nithin1217@users.noreply.github.com>
2024-03-08 01:40:11 +0530

    Update newone.sh

--- newone.sh
+++ newone.sh
@@ -2,7 +2,7 @@
 
 #'continue' statement in a loop
 for n in {1..4}; do
-if [ $n -eq 2 ]; then
+if [ $n -eq 3 ]; then
 echo "Iteration skipped for n = $n"
 continue #Skipping rest of the code in this iteration
 fi
